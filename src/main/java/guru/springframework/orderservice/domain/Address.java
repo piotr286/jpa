839a739656230b61e3cc8c859c915a0a7c7b8533
piotr <piotr286@gmail.com>
2025-11-20 02:52:52 +0100

    commit#1

--- src/main/java/guru/springframework/orderservice/domain/Address.java
+++ src/main/java/guru/springframework/orderservice/domain/Address.java
@@ -1,7 +1,7 @@
 package guru.springframework.orderservice.domain;
 
 import jakarta.persistence.Embeddable;
-import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.Size;
 
 /**
  * Created by jt on 12/11/21.
@@ -9,7 +9,7 @@ import org.hibernate.validator.constraints.Length;
 @Embeddable
 public class Address {
     private String address;
-    @Length(max = 30)
+    @Size(max = 30)
     private String city;
     private String state;
     private String zipCode;
--- src/main/java/guru/springframework/orderservice/domain/Customer.java
+++ src/main/java/guru/springframework/orderservice/domain/Customer.java
@@ -2,7 +2,8 @@ package guru.springframework.orderservice.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
-import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 
 import java.util.HashSet;
 import java.util.Objects;
@@ -29,13 +30,15 @@ import java.util.Set;
 })
 public class Customer extends BaseEntity {
 
-    @Length(max = 50)
+    @Size(max = 50)
     private String customerName;
     @Valid
     @Embedded
     private Address address;
-    @Length(max = 20)
+    @Size(max = 20)
     private String phone;
+    @Size(max = 255)
+    @Email
     private String email;
 
     @Version
